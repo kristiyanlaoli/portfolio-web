a8e437523fa1bb05260f11aa66aa0502d3ceafd5
Kristiyan <kristiyanlaoli@gmail.com>
2023-12-26 23:38:53 +0700

    add hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,17 @@
 export default {
   content: ["index.html", "src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
     extend: {
       colors: {
         primary: "#14b8a6",
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320px",
       },
     },
   },
