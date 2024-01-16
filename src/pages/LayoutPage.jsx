c269a0436dd34db8dac9e19c07023de431901cdd
Kristiyan <kristiyanlaoli@gmail.com>
2024-01-16 23:55:25 +0700

    fix import scrolltotop again

--- src/pages/LayoutPage.jsx
+++ src/pages/LayoutPage.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "/src/components/Header.jsx";
 import Footer from "/src/components/Footer.jsx";
-import ScrollToTop from "/src/components/utils/scrollToTop.jsx";
+import ScrollToTop from "/src/components/utils/ScrollToTop.jsx";
 
 const LayoutPage = () => {
   return (
