2925f9f86e0384a4be243e43d0de3a4881a707bc
Kristiyan <kristiyanlaoli@gmail.com>
2024-01-17 20:22:24 +0700

    fix router, vercel.json and linking

--- src/router.js
+++ src/router.js
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
         Component: ProjectsPage,
       },
       {
-        path: "projects/bookoe",
+        path: "bookoe",
         Component: BookoePage,
       },
     ],
